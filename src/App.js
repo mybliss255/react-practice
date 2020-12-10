608347c3e954038c7337d12452ba03f4d7f016f0
hara <mybliss1434@gmail.com>
2020-12-10 17:02:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+      return(
+        <div className="mt-5 ml-5">
+          <Button variant="primary" className="mr-2">button</Button>
+          <Button variant="secondary" className="mr-2">button</Button>
+          <Button variant="success" className="mr-2">button</Button>
+          <Button variant="warning" className="mr-2">button</Button>
+          <Button variant="danger" className="mr-2">button</Button>
+          <Button variant="info" className="mr-2">button</Button>
+        </div>
+      )
+  }
 }
-
 export default App;
